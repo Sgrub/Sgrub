24bfb5cad4fc4c2a7797dec6e44df2569f236b70
joschatom <79383126+joschatom@users.noreply.github.com>
2021-11-12 23:48:43 +0100

    update2.5

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ let html5 = document.getElementsByTagName("website");
 html5[0].innerHTML = <div>
     <p><b>Hallo</b> Welt!</p>
 </div>;
-html5[0].innerHTML = open("index.htm");
+
+class php{
+    _vars_keys = []
+    _vars_values = []
+    echo(elment) {
+        return html5[0].innerHTML += elment;
+    }
+    $(env) {
+    for (let index = 0; index < this._vars_keys; index++) {
+        if (this._vars_keys[index] == env){
+            return this._vars_values[index];
+        }
+    }
+    def(vname, value){
+      if (this._vars_keys.length != this._vars_values.length){
+          throw Error("Pares php not fund on roll_php@willdll.local.%localdomain%.orgsys.ghub");
+      }
+      this._vars_keys.push(vname);
+      this._vars_values.push(value);
+    }
+}
+
+let php = new php()
