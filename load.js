9a3d315f64494c8ab45341cdbef31c130b1ee835
joschatom <79383126+joschatom@users.noreply.github.com>
2021-11-12 22:11:00 +0100

    crate website1
    websit.js initing

---
+++ load.js
@@ -0,0 +1,17 @@
+let website = document.body;
+
+class index{
+    html5 = document.getElementsByName("root");
+    load() {
+        return <website>
+            kjsakljd
+        </website>
+    }
+}
+
+function GET_1(uri="log:winres://index.website") {
+    if (uri == "log:winres://index.website"){
+        let doc = new index()
+        website.getElementsByName("root").inerHTML +=  doc.load()
+    }
+}
